b7be6ba3d97f2f1a09c7c282f0ea755d1e74c5e0
valentimarco <gamespy67@gmail.com>
2024-03-25 11:07:13 +0100

    v0.0.1

---
+++ dynamic_language.py
@@ -0,0 +1,20 @@
+from cat.mad_hatter.decorators import hook
+
+
+@hook(priority=2)
+def before_cat_reads_message(user_message_json: dict, cat) -> dict:
+    if "lang" in user_message_json:
+        cat.working_memory["lang"] = user_message_json["lang"]
+    return user_message_json
+
+
+@hook(priority=0)
+def agent_prompt_suffix(suffix, cat):
+    if "lang" in cat.working_memory:
+        lang = cat.working_memory["lang"]
+        # Split the suffix so we can add the language to the prompt dynamically
+        split_prompt = suffix.split("## Conversation until now:")
+        split_prompt[0] = f"{split_prompt[0]}ALWAYS answer in {lang}\n\n"
+
+        suffix = split_prompt[0] + "## Conversation until now:" + split_prompt[1]
+    return suffix
